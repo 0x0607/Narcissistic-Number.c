8e4686e08531b7e511dac5962997636dc81cedf9
T3oSr.Coding <74230665+0x0607@users.noreply.github.com>
2021-11-02 02:06:35 +0800

    Narcissistic-Number^3

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    unsigned int input=0,output=0,temp=0;
+    int i=1;
+    while(input<100||input>999){
+        printf("plz type Ur input：");
+        scanf("%d",&input);
+    }
+    while(i<1000){
+        temp=input/i%10;
+        output+=temp*temp*temp;
+        printf("\n%d^3=%d",temp,temp*temp*temp);
+    i*=10;}
+    printf("\n\n%d is %sNarcissistic-Number",input,output==input ? "\0" :"not ");
+return 0;}
+/************************************/
+//水仙花數(Narcissistic number)定義：
+//其各個數之立方和等於該數
+/************************************/
